3f321b4ef2ac9b8b2414f995644568420542f293
adithya <adithyaskumar001@gmail.com>
2022-11-03 17:27:05 +0530

    created show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,19 @@
 class ArticlesController < ApplicationController
 
-	def index
-		@article = Article.all
-	end
 	def new
 		@article = Article.new
-	end
-
-	def show
-		@article = Article.find(params[:id])
 	end
 
 	def create
 		@article = Article.new(article_params)
 		if @article.save
-			# flash[:notice] = "Article successfully created"
 			redirect_to @article
 		else
-			render :new, status: :unprocessable_entity
+			render 'new'
 		end
+	end
+	def show
+		@article = Article.find(params[:id])
 	end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'welcome#home'
+  root 'articles#new'
   get 'welcome/about', to: 'welcome#about'
 
   resources :articles
